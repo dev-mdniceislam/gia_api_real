de39fb6214eea67486a1fc212c159b61108c5926
Nice Islam <abusayeed7710@gmail.com>
2026-09-22 23:50:11 +0600

    update teacher routes

--- src/routes/teacherRoutes/teacherRoutes.js
+++ src/routes/teacherRoutes/teacherRoutes.js
@@ -4,28 +4,24 @@ const { upload } = require('../../middlewares/multerMiddleware');
 const isValidToken = require('../../middlewares/authMiddleware');
 
 const {
-  getGalleryDataAll,
-  createCategory,
-  getImagesByCategoryId,
-  pushImagesToCategory,
-  deleteCategory,
-  deleteSingleImageFromCategory,
-} = require('../../controllers/galleryController/galleryController');
+  getAllTeachers,
+  getTeacherById,
+  createTeacher,
+  updateTeacherInfoById,
+  deleteTeacherById,
+} = require('../../controllers/teacherController/teacherController');
 
 // Base route: /api/v1/gallery
 router
   .route('/')
-  .get(getGalleryDataAll)
-  .post(isValidToken, upload.array('images', 10), createCategory);
+  .get(getAllTeachers)
+  .post(isValidToken, upload.single('image'), createTeacher);
 
 // Category route by ID: /api/v1/gallery/:id
 router
   .route('/:id')
-  .get(getImagesByCategoryId)
-  .post(isValidToken, upload.array('images', 10), pushImagesToCategory)
-  .delete(isValidToken, deleteCategory);
-
-// Delete single image route: /api/v1/gallery/:id/delete-image
-router.delete('/:id/delete-image', isValidToken, deleteSingleImageFromCategory);
+  .get(getTeacherById)
+  .put(isValidToken, upload.single('image'), updateTeacherInfoById)
+  .delete(isValidToken, deleteTeacherById);
 
 module.exports = router;
